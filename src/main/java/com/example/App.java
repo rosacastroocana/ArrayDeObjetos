c7b47848162ce6d2e5ce831e3aa1497dd5dd3287
rosacastroocana <rosa.castro.ocana@gmail.com>
2024-05-31 14:02:51 +0200

    ejercicio 2 terminado promedio

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -50,17 +50,51 @@ public class App
     	double salarioPromedio = 0.0;
 
 	for(Persoona persona : personas) {
-		sumatoriaDeLosSalarios = sumatoriaDeLosSalarios + persona.getSalario();
+//		sumatoriaDeLosSalarios = sumatoriaDeLosSalarios + persona.getSalario();
 		sumatoriaDeLosSalarios += persona.getSalario();
 		
 	}
 	
-	salarioPromedio = sumatoriaDeLosSalarios / totalDePersonas;
-		System.out.println("SALARIO PROMEDIO MUJERES ------" +salarioPromedio);	
-
+//	salarioPromedio = sumatoriaDeLosSalarios / totalDePersonas;
+//		System.out.println("SALARIO PROMEDIO MUJERES ------" +salarioPromedio);	
+//		
+//		
+		// Ejercicio 2 recorrer el array de personas y calcular el salario 
+		// promedio por genero
 		
+		
+    	double sumatoriaSalariosHombre = 0.0;
+    	double sumatoriaSalariosMujer = 0.0;
+    	double sumatoriaSalariosOtro = 0.0;
+    	
+    	int totalGeneroHombre = 0;
+    	int totalGeneroMujer = 0;
+    	int totalGeneroOtro = 0;
+    	
+    	double salarioPromedioHombres = 0.0;
+    	double salarioPromedioMujeres = 0.0;
+    	double salarioPromedioOtros = 0.0;
+
+	for(Persoona p : personas) {
+		if(p.getGenero().equals(Genero.HOMBRE)) {
+		sumatoriaSalariosHombre += p.getSalario();
+		totalGeneroHombre++;
+		} else if(p.getGenero().equals(Genero.MUJER)) {
+		sumatoriaSalariosMujer += p.getSalario();
+		totalGeneroMujer++;				
+		} else {
+		sumatoriaSalariosOtro += p.getSalario();
+		totalGeneroOtro++;
+		}
+   
+}
+  	salarioPromedioHombres = sumatoriaSalariosHombre / totalGeneroHombre;
+  	salarioPromedioMujeres = sumatoriaSalariosMujer / totalGeneroMujer;
+  	salarioPromedioOtros = sumatoriaSalariosOtro / totalGeneroOtro;
+  	
+      	
+      	System.out.println("Salario promedio de los Hombres: " + salarioPromedioHombres);
+      	System.out.println("Salario promedio de las Mujeres: " + salarioPromedioMujeres);
+      	System.out.println("Salario promedio de Otros Generos: " + salarioPromedioOtros);
     }
 }
-
-
-
